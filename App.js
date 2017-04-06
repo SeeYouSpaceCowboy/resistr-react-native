685bfff5d1bd5151eeb5fb234c8a965d0e4be737
Cory <cory.forsyth@gmail.com>
2017-04-05 20:15:08 -0400

    Add react-native-material-ui
    Adds a (useless) "Search" toolbar in the center of the screen

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, NativeModules, Navigator, StatusBar } from 'react-native';
+import { COLOR, ThemeProvider, Toolbar } from 'react-native-material-ui';
+
+const UIManager = NativeModules.UIManager;
+
+const uiTheme = {
+    palette: {
+        primaryColor: COLOR.green500,
+    },
+    toolbar: {
+        container: {
+            height: 50,
+        },
+    },
+};
 
 export default class App extends React.Component {
+	componentWillMount() {
+			if (UIManager.setLayoutAnimationEnabledExperimental) {
+					UIManager.setLayoutAnimationEnabledExperimental(true);
+			}
+	}
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ThemeProvider uiTheme={uiTheme}>
+        <View style={styles.container}>
+					<Toolbar
+						leftElement="menu"
+						centerElement="Searchable"
+						searchable={{
+							autoFocus: true,
+							placeholder: 'Search',
+						}}
+          />
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar backgroundColor="rgba(0, 0, 0, 0.8)" translucent />
+        </View>
+      </ThemeProvider>
     );
   }
 }
