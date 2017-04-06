95e5c8e33e05ec382d4a50b4e58c4eb2e6344cbc
Chirag Patel <patelc75@yahoo.com>
2017-04-05 21:15:12 -0400

    added cards

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, NativeModules, Navigator, StatusBar } from 'react-native';
-import { COLOR, ThemeProvider, Toolbar, ActionButton } from 'react-native-material-ui';
+import { COLOR, ThemeProvider, Toolbar, ActionButton, Drawer, Avatar, Card, ListItem } from 'react-native-material-ui';
 
 const UIManager = NativeModules.UIManager;
 
@@ -25,7 +25,8 @@ export default class App extends React.Component {
   render() {
     return (
       <ThemeProvider uiTheme={uiTheme}>
-        <ActionButton
+        <View style={styles.container}>
+          <ActionButton
             actions={[
                 { icon: 'call', label: 'phone call' },
                 { icon: 'voicemail', label: 'voice message' },
@@ -38,7 +39,37 @@ export default class App extends React.Component {
             ]}
             transition="speedDial"
             onPress={(action) => console.log('pressed',action)}
-        />
+          />
+          <Card>
+              <ListItem
+                  leftElement={<Avatar text="MW" />}
+                  centerElement={{
+                      primaryText: 'Donald Trump',
+                      secondaryText: 'POTUS',
+                  }}
+              />
+              <View style={styles.textContainer}>
+                  <Text>
+                      45th President of the United States and star of the Apprentice and author of The Art of the Deal and owner of several toupees and the best dude in the world. And also need to make this text very long
+                  </Text>
+              </View>
+          </Card>
+          <Card>
+              <ListItem
+                  leftElement={<Avatar text="MW" />}
+                  centerElement={{
+                      primaryText: 'Mike Pence',
+                      secondaryText: 'Vice President',
+                  }}
+              />
+              <View style={styles.textContainer}>
+                  <Text>
+                      Vice President of the United States and star of the Apprentice and author of The Art of the Deal and owner of several toupees and the best dude in the world. And also need to make this text very long
+                  </Text>
+              </View>
+          </Card>          
+          <StatusBar backgroundColor="rgba(0, 0, 0, 0.8)" translucent />
+        </View>
       </ThemeProvider>
     );
   }
