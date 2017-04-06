4063c0fef2686396d3b0a6154873fc179a25b5b3
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-04-06 12:20:13 -0400

    Added API response

--- App.js
+++ App.js
@@ -1,14 +1,74 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, Alert, PushNotificationIOS } from 'react-native';
 
-export default class App extends React.Component {
+const URL = "http://localhost:3000/api/representatives?address=%22735%20jefferson%20ave%20brooklyn%20ny%22"
+
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      json: "Nothing"
+    }
+  }
+
+  componentDidMount(){
+    this.fetchData().done()
+  }
+
+  componentWillMount() {
+    PushNotificationIOS.addEventListener('notification', this._onNotification);
+  }
+
+  componentWillUnmount() {
+    PushNotificationIOS.removeEventListener('notification', this._onNotification);
+  }
+
+
+  onButtonPress = () => {
+    Alert.alert(this.state.json);
+  }
+
+  async fetchData() {
+    const response = await fetch(URL)
+    const json = await response.json()
+    const city = json.address.city
+    this.setState({ json: city })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          onPress={ this.onButtonPress }
+          title={ this.state.json }
+          color="#841584" />
       </View>
+    );
+  }
+
+  _sendNotification() {
+    require('RCTDeviceEventEmitter').emit('remoteNotificationReceived', {
+      aps: {
+        alert: 'Sample notification',
+        badge: '+1',
+        sound: 'default',
+        category: 'REACT_NATIVE'
+      },
+    });
+  }
+
+  _onNotification(notification) {
+    AlertIOS.alert(
+      'Notification Received',
+      'Alert message: ' + notification.getMessage(),
+      [{
+        text: 'Dismiss',
+        onPress: null,
+      }]
     );
   }
 }
